2455c0cf7e75430b64ee96183cfbe3c215e17dbe
Jose Ricardo <josetecnom12@hotmail.com>
2018-02-07 19:52:39 -0500

    File finder initialized

--- LanguajeDivider.py
+++ LanguajeDivider.py
@@ -2,6 +2,14 @@
 import sys
 
 def main():
-    directory = raw_input("Locacion del archivo: ")
+    archivo = " "
+    try:
+        open("Languajes.txt", "r+")
+    except:
+        directory = raw_input("Locacion del archivo: ")
+        try:
+            archivo = open(directory, "r+")
+        except:
+            print("No se a encontrado el archivo")
 if __name__ == '__main__':
     main()
