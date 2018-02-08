5ee986b7218209b0b6f5b1b3245f37eb097b8351
Jose Ricardo <josetecnom12@hotmail.com>
2018-02-07 20:40:58 -0500

    Functional file selector, with line organizer

--- LanguajeDivider.py
+++ LanguajeDivider.py
@@ -1,15 +1,22 @@
 #!/bin/python2
+from __future__ import print_function
 import sys
 
 def main():
-    archivo = " "
+    archivo = None
     try:
-        open("Languajes.txt", "r+")
+        archivo = open("Languajes.txt", "r+")
     except:
         directory = raw_input("Locacion del archivo: ")
         try:
             archivo = open(directory, "r+")
         except:
             print("No se a encontrado el archivo")
+    lines = 0
+    for line in archivo:
+        print(lines, end='')
+        print("> ", end='')
+        print(line, end='')
+        lines += 1
 if __name__ == '__main__':
     main()
